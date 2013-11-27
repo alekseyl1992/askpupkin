86730b8ba50f5655c9318cd2b58dd8ae47cfe8eb
alekseyl1992 <alekseyl@mail.ru>
2013-11-28 02:43:15 +0400

    memcaching for tags added

--- ask/context_processors.py
+++ ask/context_processors.py
@@ -1,26 +1,39 @@
 from django.core.context_processors import request
 import queries
 import random
+import memcache
+import json
 
 
 def get_randomized_tags():
-    popular_tags = queries.get_popular_tags()
+    mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+    data = mc.get("tags")
 
-    if popular_tags.count() == 0:
-        return []
+    if data:
+        return json.loads(data)
+    else:
+        popular_tags = queries.get_popular_tags()
 
-    max_count = popular_tags[0].quest_count
-    min_count = popular_tags[len(popular_tags)-1].quest_count
-    count_dif = max_count - min_count
-    weight_count = 5
+        if popular_tags.count() == 0:
+            return []
 
-    popular_tags_list = list(popular_tags)
-    random.shuffle(popular_tags_list)
+        max_count = popular_tags[0].quest_count
+        min_count = popular_tags[len(popular_tags)-1].quest_count
+        count_dif = max_count - min_count
+        weight_count = 5
 
-    for tag in popular_tags:
-        tag.weight = tag.quest_count % count_dif * weight_count / count_dif + 1
+        #popular_tags_list = list(popular_tags)
+        #random.shuffle(popular_tags_list)
 
-    return popular_tags_list
+        tags_dict = {}
+        for tag in popular_tags:
+            weight = tag.quest_count % count_dif * weight_count / count_dif + 1
+            tags_dict[tag.name] = weight
+
+        data = json.dumps(tags_dict)
+        mc.set("tags", data, 60*5)
+
+        return tags_dict
 
 
 def tags(request):
